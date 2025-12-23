a5b0c901840e3e7f1eecb035b4c03a3a656f8330
Bakhodir201 <komolovboxodir2703@gmail.com>
2025-12-23 17:37:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let name = prompt("ismingiz nma")
+
+let age = prompt("Yoshiz nechida")
+
+let year = prompt("tugilgan yiliz")
+
+let from = prompt("Qaysi davlatda tugilgansiz")
+
+console.log("Ismiz",name,"yoshingiz",age,"yilingiz",year,"Davaltingiz",from)
